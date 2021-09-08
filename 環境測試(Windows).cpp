02b03aa6eebbef7dc77cefad130a7ec3ff800f20
smitug01 <sumily20@gmail.com>
2021-09-08 21:42:35 +0800

    Update 環境測試(Windows).cpp

--- 環境測試(Windows).cpp
+++ 環境測試(Windows).cpp
@@ -1,32 +1,17 @@
-{\rtf1\ansi\ansicpg950\cocoartf2622
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;\f1\fnil\fcharset136 PingFangTC-Regular;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
-\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+#include <iostream>
+#include <iomanip>
+using std::cout;
+using std::endl;
 
-\f0\fs24 \cf0 #include <iostream>\
-#include <iomanip>\
-using std::cout;\
-using std::endl;\
-\
-main()\
-\{\
-    int v1,v2,temp;\
-    \
-    v1=12;v2=36;\
-    cout<<"
-\f1 \'bc\'c6\'a6\'72\'a5\'e6\'b4\'ab\'ab\'65
-\f0  :v1="<<v1<<"
-\f1 \'a1\'41
-\f0 v2="<<v2<<endl;\
-    temp=v1;\
-    v1=v2;\
-    v2=temp;\
-    cout<<"
-\f1 \'bc\'c6\'a6\'72\'a5\'e6\'b4\'ab\'ab\'e1
-\f0  :v1="<<v1<<"
-\f1 \'a1\'41
-\f0 v2="<<v2<<endl;\
-    system("Pause");\
-\}}
+main()
+{
+    int v1,v2,temp;
+    
+    v1=12;v2=36;
+    cout<<"數字交換前 :v1="<<v1<<"，v2="<<v2<<endl;
+    temp=v1;
+    v1=v2;
+    v2=temp;
+    cout<<"數字交換後 :v1="<<v1<<"，v2="<<v2<<endl;
+    system("Pause");
+}
