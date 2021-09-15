d6a1543d3cc4cde3e5d729293387b6f10e11db3a
smitug01 <sumily20@gmail.com>
2021-09-15 21:44:37 +0800

    Create a001.cpp

---
+++ zerojudge/a001.cpp
@@ -0,0 +1,19 @@
+//
+//  a001.cpp
+//  Project
+//
+//  Created by LU,CHENG-HUNG on 2021/9/15.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string name;
+cin >> name;
+printf("hello, ");
+cout << name << endl;
+//    system("pause");
+    return 0;
+}
