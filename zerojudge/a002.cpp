c80c9de90f7019762cd9ab5fcf664b4ffd041b49
smitug01 <sumily20@gmail.com>
2021-09-15 21:45:42 +0800

    Create a002.cpp

---
+++ zerojudge/a002.cpp
@@ -0,0 +1,16 @@
+//
+//  a002.cpp
+//  Project
+//
+//  Created by LU,CHENG-HUNG on 2021/9/15.
+//
+#include <iostream>
+using namespace std;
+int main ()
+{
+int a,b;
+while (cin>> a >> b )
+cout<<a+b;
+//    system("Pause");
+return 0;
+}
